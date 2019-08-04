36af7b407b01259454318c94f4542cab4426c8b4
Timur860 <timur860@mail.ru>
2019-08-04 15:59:15 +0300

    version 2.0

--- src/main/java/ru/timur860/students/model/Student.java
+++ src/main/java/ru/timur860/students/model/Student.java
@@ -1,20 +1,14 @@
 package ru.timur860.students.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import java.math.BigDecimal;
 
 @Entity
 @Table(name = "students")
-@Getter
-@Setter
-@ToString
+
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Student extends BaseEntity {
     @Column(name = "first_name")
@@ -28,5 +22,47 @@ public class Student extends BaseEntity {
 
     @Column(name = "birth_year")
     private int birthYear;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", course='" + course + '\'' +
+                ", birthYear=" + birthYear +
+                '}';
+    }
 
 }
--- src/main/java/ru/timur860/students/service/StudentServiceImpl.java
+++ src/main/java/ru/timur860/students/service/StudentServiceImpl.java
@@ -1,6 +1,6 @@
 package ru.timur860.students.service;
 
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.timur860.students.model.Student;
@@ -8,33 +8,29 @@ import ru.timur860.students.repository.StudentRepository;
 
 import java.util.List;
 
-@Slf4j
+
 @Service
-public class StudentServiceImpl implements StudentService{
+public class StudentServiceImpl implements StudentService {
     @Autowired
     StudentRepository StudentRepository;
 
     @Override
     public Student getById(Long id) {
-        log.info("IN StudentServiceImpl getById {}", id);
         return StudentRepository.getOne(id);
     }
 
     @Override
     public void save(Student student) {
-        log.info("IN StudentServiceImpl save {}", student);
         StudentRepository.save(student);
     }
 
     @Override
     public void delete(Long id) {
-        log.info("IN StudentServiceImpl delete {}", id);
         StudentRepository.deleteById(id);
     }
 
     @Override
     public List<Student> getAll() {
-        log.info("IN StudentServiceImpl getAll");
         return StudentRepository.findAll();
     }
 
